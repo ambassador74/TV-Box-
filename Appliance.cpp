9a0f0ee2e590ac3acbb65bc85f398086a403b903
ambassador74 <collabus@list.ru>
2024-06-11 12:26:10 +0300

    interface has been written

--- Appliance.cpp
+++ Appliance.cpp
@@ -4,5 +4,17 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    unsigned int channel, volume;
+    bool isOn;
+    std::cout << "Please input channel, volume and set turn on/turn off through space" << std::endl;
+    std::cin >> channel >> volume >> isOn;  
+    Box TV;
+    TV.turnOn(isOn);
+    if (TV.getIsOn() == true)
+    {
+        TV.switchVolume(volume);
+        TV.switchChannel(channel);
+    }
+    std::cout << "Параметры: " << TV.getVolume() << " " << TV.getVolume() << " " << TV.getIsOn << std::endl;
+    return 0;
 }
